6d09f32ac04c348ca7cf180296dd6a724c419fad
Igor Câmara <igor.phelype@gmail.com>
2021-07-21 18:30:42 -0300

    testing react lazy

--- packages/main-module/src/App.tsx
+++ packages/main-module/src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import PomoModuleApp from "pomo-module/src/PomoModuleApp";
+const PomoModuleApp = React.lazy(() => import("pomo-module/src/PomoModuleApp"));
 import TodoListModuleApp from "todolist-module/src/TodoListModuleApp";
 import { WindowModule } from "shared-types";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import "./index.css";
 
@@ -19,7 +20,33 @@ window.headermodule.get("./src/Header").then((module) => {
 const App = () => (
   <>
     <section>
-      <PomoModuleApp />
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/pomo">pomo</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/pomo">
+              <React.Suspense fallback={<div>carregando...</div>}>
+                <PomoModuleApp />
+              </React.Suspense>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
       <TodoListModuleApp />
     </section>
   </>
